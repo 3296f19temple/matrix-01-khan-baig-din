b041f256fdb7fed29da16b5d7ecf9a0b9b396b7b
Dawud Baig <dawudi284@gmail.com>
2019-10-24 23:14:09 -0400

    fixing, almost done

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -9,14 +9,18 @@ double* gen_matrix(int n, int m);
 int mmult(double *c, double *a, int aRows, int aCols, double *b, int bRows, int bCols);
 void compare_matrix(double *a, double *b, int nRows, int nCols);
 
+// Declares Functions
+double** importMatrix(FILE *fp, int numRow,int numCol);
+void printMatrix(double** matrix,int numCol,int numRow);
+
 /** 
     Program to multiply a matrix times a matrix using both
     mpi to distribute the computation among nodes and omp
     to distribute the computation among threads.
 */
 
-int main(int argc, char* argv[])
-{
+  int main(int argc, char* argv[])
+  {
   int nrows, ncols;
   double *aa;	/* the A matrix */
   double *bb;	/* the B matrix */
@@ -26,7 +30,15 @@ int main(int argc, char* argv[])
   double starttime, endtime;
   MPI_Status status;
   /* insert other global variables here */
-  MPI_Init(&argc, &argv);
+
+    char ch;
+    FILE *fp1, *fp2, *fpOut;
+    int aRow, aCol, bRow, bCol;
+    char str1[10], str2[10];
+    double matrixInsert;
+    int canMult = 0;
+
+    MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
   if (argc > 1) {
@@ -36,8 +48,68 @@ int main(int argc, char* argv[])
       // Master Code goes here
       aa = gen_matrix(nrows, ncols);
       bb = gen_matrix(ncols, nrows);
-      cc1 = malloc(sizeof(double) * nrows * nrows); 
-      starttime = MPI_Wtime();
+      cc1 = malloc(sizeof(double) * nrows * nrows);
+
+        //Import matrix a dimensions
+        if ((fp1 = fopen(argv[ 1 ], "r")) == NULL) {
+            printf("file cannot be opened.\n");
+            exit(1);
+        }
+        fscanf(fp1, "%s", str1);
+        fscanf(fp1, "%s", str1);
+        aRow = atoi(str1);
+
+        printf("aRow = %d\n", aRow);
+
+        fscanf(fp1, "%s", str2);
+        fscanf(fp1, "%s", str2);
+        fscanf(fp1, "%s", str2);
+
+        aCol = atoi(str2);
+        printf("aCol = %d\n", aCol);
+        // gets rid of ")"
+        fscanf(fp1, "%s", str2);
+
+        //Import matrix b dimensions
+        if ((fp2 = fopen(argv[ 2 ], "r")) == NULL) {
+            printf("file cannot be opened.\n");
+            exit(1);
+        }
+        fscanf(fp2, "%s", str1);
+        fscanf(fp2, "%s", str1);
+        bRow = atoi(str1);
+        printf("bRow = %d\n", bRow);
+
+        fscanf(fp2, "%s", str2);
+        fscanf(fp2, "%s", str2);
+        fscanf(fp2, "%s", str2);
+        bCol = atoi(str2);
+        printf("bCol = %d\n", bCol);
+        // gets rid of ")"
+        fscanf(fp2, "%s", str2);
+
+        //Check is matrix a and b can be multiplied
+        if (aCol == bRow) {
+            canMult = 1;
+            printf("matrices can be multiplied");
+        } else {
+            printf("matrices can NOT be multiplied");
+            fclose(fp1);
+            fclose(fp2);
+            return 0;
+        }
+
+        aa = importMatrix(fp1,aCol,aRow);
+        printf("\nMatrix aa: ");
+        printMatrix(aa,aCol,aRow);
+
+        bb = importMatrix(fp2,bCol,bRow);
+        printf("\nMatrix bb: ");
+        printMatrix(bb,bCol,bRow);
+
+        starttime = MPI_Wtime();
+        sent = 0;
+
       /* Insert your master code here to store the product into cc1 */
       endtime = MPI_Wtime();
       printf("%f\n",(endtime - starttime));
@@ -52,4 +124,31 @@ int main(int argc, char* argv[])
   }
   MPI_Finalize();
   return 0;
-}
+  }
+  double** importMatrix(FILE *fp, int numCol,int numRow) {
+
+    int i, j;
+    double** matrix = malloc(numCol * sizeof (double*));
+    for (i = 0; i < numCol; ++i)
+        matrix[i] = malloc(numRow * sizeof (double));
+
+    for (i = 0; i < numCol; i++) {
+        for (j = 0; j < numRow; j++) {
+            if (!fscanf(fp, "%lf", &matrix[i][j]))
+                break;
+        }
+    }
+    close(fp);
+    return matrix;
+  }
+
+
+  void printMatrix(double** matrix,int numCol,int numRow){
+    int i,j;
+    for (i = 0; i < numCol; i++) {
+        printf("\n");
+        for (j = 0; j < numRow; j++) {
+            printf("mat[%d][%d] = %lf ", i, j, matrix[i][j]);
+        }
+    }
+  }
